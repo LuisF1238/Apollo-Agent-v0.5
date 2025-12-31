5eead2c891eb9caab24f35a38068ddef37ec4feb
LaurenMaiH <lauriedosan@gmail.com>
2025-12-31 12:38:02 -0800

    added title selection to distribute results across evenly

---
+++ config/title_groups.py
@@ -0,0 +1,28 @@
+TITLE_GROUPS = {
+    "Project Manager": [
+        "Project Manager",
+        "Program Manager",
+        "Delivery Manager",
+        "Implementation Manager"
+    ],
+    "Product": [
+        "Product Manager",
+        "Senior Product Manager",
+        "Product Lead",
+        "Head of Product"
+    ],
+    "Director": [
+        "Director",
+        "Director of Data Science",
+        "Director of Analytics",
+        "Director of Engineering",
+        "Director of Product"
+    ],
+    "VP": [
+        "VP",
+        "Vice President",
+        "VP Data",
+        "VP Analytics",
+        "VP Engineering"
+    ]
+}
